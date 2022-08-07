7d2ddfd826d576ba104561cbf1d5a6c2e6731d6d
dremkay <dremkay71@gmail.com>
2022-08-07 23:45:01 +0100

    count added

--- fizzBuzz/fizzbuzz.js
+++ fizzBuzz/fizzbuzz.js
@@ -6,20 +6,27 @@
 
 
 function fizzBuzz() {
-    
+    let fizzCount = 0
+    let buzzCount = 0
+    let fizzBuzzCount = 0
     userInput = parseInt(prompt("Enter an Integer"))
     for (i=1;i<=userInput;i++){
         if (i%3===0 && i%5===0){
+            fizzBuzzCount++
             console.log("fizzBuzz")
         } else if (i%3===0){
+            fizzCount++
             console.log("fizz")
         } else if (i%5===0){
+            buzzCount++
             console.log("Buzz")
         }else{
             console.log(i)
-        }
-        
+        }        
     }
+    console.log(`fizz: ${(fizzCount /userInput)*100}%
+    Buzz: ${(buzzCount /userInput)*100}%
+    fizzBuzz: ${(fizzBuzzCount /userInput)*100}%`)
 }
 
 fizzBuzz()
